c62b3400805526a08c226aa4dd28a0d6e167e8ba
kghosh <kghosh@moneylion.com>
2024-10-28 12:28:26 +0800

    updated the external service setup and componentscan

--- EventProducerWeb/src/main/java/com/example/eventwebproducer/EventWebProducer.java
+++ EventProducerWeb/src/main/java/com/example/eventwebproducer/EventWebProducer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.service"})
+@ComponentScan(basePackages = {"com.example.service", "com.example.eventwebproducer"})
 public class EventWebProducer {
 
     public static void main(String[] args) {
